6998e81cef6ef247cc7a12155bcbf17228eb11d3
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2023-01-31 17:59:01 +0000

    Connect sanity backend to react frontend and output about section data

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,29 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-import { images } from "../../constants";
 import "./About.scss";
-
-const abouts = [
-  {
-    title: "Web Development",
-    description: "I am good",
-    imgUrl: images.about01,
-  },
-  {
-    title: "Frontend Development",
-    description: "I am good",
-    imgUrl: images.about02,
-  },
-  {
-    title: "Fullstack Development",
-    description: "I am good",
-    imgUrl: images.about04,
-  },
-  { title: "UI/UX", description: "I am good", imgUrl: images.about03 },
-];
+import { urlFor, client } from "../../client";
 
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
+
   return (
     <>
       <h2 className="head-text">
@@ -41,7 +32,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
               {about.title}
             </h2>
