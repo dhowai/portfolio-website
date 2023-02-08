afa8ad22032169fe7df3a219166e818b5643b1b4
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2023-02-08 14:59:16 +0000

    Add conditional rendering on work and make hover more visable

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { AiFillEye, AiFillGithub } from "react-icons/ai";
 import { motion } from "framer-motion";
 
+import { images } from "../../constants";
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { urlFor, client } from "../../client";
 import "./Work.scss";
@@ -43,75 +44,95 @@ const Work = () => {
       </h2>
 
       <div className="app__work-filter">
-        {["HTML/CSS", "JavaScript", "Python", "Django", "Ecommerce", "All"].map(
-          (item, index) => (
-            <div
-              key={index}
-              onClick={() => handleWorkFilter(item)}
-              className={`app__work-filter-item app__flex p-text ${
-                activeFilter === item ? "item-active" : ""
-              }`}
-            >
-              {item}
-            </div>
-          )
-        )}
-      </div>
-      <motion.div
-        animate={animateCard}
-        transition={{ duration: 0.5, delaychildren: 0.5 }}
-        className="app__work-portfolio"
-      >
-        {filterWork.map((work, index) => (
-          <div className="app__work-item app__flex" key={index}>
-            <div className="app__work-img app__flex">
-              <img src={urlFor(work.imgUrl)} alt={work.name} />
-              <motion.div
-                whileHover={{ opacity: [0, 1] }}
-                transition={{
-                  duration: 0.25,
-                  ease: "easeInOut",
-                  staggerChildren: 0.5,
-                }}
-                className="app__work-hover app__flex"
-              >
-                <a href={work.projectLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    whileHover={{ opacity: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillEye />
-                  </motion.div>
-                </a>
-
-                <a href={work.codeLink} target="_blank" rel="noreferrer">
-                  <motion.div
-                    whileInView={{ opacity: [0, 1] }}
-                    whileHover={{ opacity: [1, 0.9] }}
-                    transition={{ duration: 0.25 }}
-                    className="app__flex"
-                  >
-                    <AiFillGithub />
-                  </motion.div>
-                </a>
-              </motion.div>
-            </div>
-
-            <div className="app__work-content app__flex">
-              <h4 className="bold-text">{work.title}</h4>
-              <p className="p-text" style={{ marginTop: 10 }}>
-                {work.description}
-              </p>
-
-              <div className="app__work-tag app__flex">
-                <p className="p-text">{work.tags[0]}</p>
-              </div>
-            </div>
+        {[
+          "HTML/CSS",
+          "JavaScript",
+          "Python",
+          "Django",
+          "Ecommerce",
+          "React.js",
+          "Node.js",
+          "All",
+        ].map((item, index) => (
+          <div
+            key={index}
+            onClick={() => handleWorkFilter(item)}
+            className={`app__work-filter-item app__flex p-text ${
+              activeFilter === item ? "item-active" : ""
+            }`}
+          >
+            {item}
           </div>
         ))}
-      </motion.div>
+      </div>
+
+      {filterWork.length >= 1 ? (
+        <motion.div
+          animate={animateCard}
+          transition={{ duration: 0.5, delaychildren: 0.5 }}
+          className="app__work-portfolio"
+        >
+          {filterWork.map((work, index) => (
+            <div className="app__work-item app__flex" key={index}>
+              <div className="app__work-img app__flex">
+                <img src={urlFor(work.imgUrl)} alt={work.name} />
+                <motion.div
+                  whileHover={{ opacity: [0, 1] }}
+                  transition={{
+                    duration: 0.25,
+                    ease: "easeInOut",
+                    staggerChildren: 0.5,
+                  }}
+                  className="app__work-hover app__flex"
+                >
+                  <a href={work.projectLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      whileHover={{ opacity: [1, 0.7] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+
+                  <a href={work.codeLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ opacity: [0, 1] }}
+                      whileHover={{ opacity: [1, 0.7] }}
+                      transition={{ duration: 0.25 }}
+                      className="app__flex"
+                    >
+                      <AiFillGithub />
+                    </motion.div>
+                  </a>
+                </motion.div>
+              </div>
+
+              <div className="app__work-content app__flex">
+                <h4 className="bold-text">{work.title}</h4>
+                <p className="p-text" style={{ marginTop: 10 }}>
+                  {work.description}
+                </p>
+
+                <div className="app__work-tag app__flex">
+                  <p className="p-text">{work.tags[0]}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </motion.div>
+      ) : (
+        <motion.div
+          animate={animateCard}
+          transition={{ duration: 0.5 }}
+          className="app__work-portfolio"
+        >
+          <div className="app__work-img app__flex">
+            <img src={images.workinprog} alt="Work In Progress" />
+          </div>
+        </motion.div>
+      )}
     </>
   );
 };
