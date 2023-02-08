d325f5f95bf8f3e2da599e7b519b4aa54fe60173
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2023-02-08 15:03:48 +0000

    Add and remove images

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,22 +1,14 @@
 import email from "../assets/email.png";
 import mobile from "../assets/mobile.png";
-import api from "../assets/api.png";
-import cpp from "../assets/cpp.png";
 import css from "../assets/css.png";
-import figma from "../assets/figma.png";
-import flutter from "../assets/flutter.png";
 import git from "../assets/git.png";
-import graphql from "../assets/graphql.png";
 import html from "../assets/html.png";
 import javascript from "../assets/javascript.png";
-import mu5 from "../assets/mu5.png";
 import node from "../assets/node.png";
 import python from "../assets/python.png";
 import react from "../assets/react.png";
-import redux from "../assets/redux.png";
 import sass from "../assets/sass.png";
 import typescript from "../assets/typescript.png";
-import vue from "../assets/vue.png";
 
 import about1 from "../assets/about1.jpg";
 import about2 from "../assets/about2.jpg";
@@ -29,35 +21,22 @@ import profile from "../assets/profile.png";
 import profile1 from "../assets/profile1.png";
 
 import logo from "../assets/logo.png";
+import workinprog from "../assets/workinprog.png";
 
-import adidas from "../assets/adidas.png";
 import amazon from "../assets/amazon.png";
-import asus from "../assets/asus.png";
-import bolt from "../assets/bolt.png";
-import nb from "../assets/nb.png";
-import skype from "../assets/skype.png";
-import spotify from "../assets/spotify.png";
 
 export default {
   email,
   mobile,
-  api,
-  cpp,
   css,
-  figma,
-  flutter,
   git,
-  graphql,
   html,
   javascript,
-  mu5,
   node,
   python,
   react,
-  redux,
   sass,
   typescript,
-  vue,
   about1,
   about2,
   about3,
@@ -67,11 +46,6 @@ export default {
   profile,
   profile1,
   logo,
-  adidas,
   amazon,
-  asus,
-  bolt,
-  nb,
-  skype,
-  spotify,
+  workinprog,
 };
