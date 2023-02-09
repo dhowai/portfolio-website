3eb612ddc4db611564e5814dbf8ddae9781dd53f
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2023-02-09 15:44:07 +0000

    Update imports and exports

--- frontend_react/src/components/index.js
+++ frontend_react/src/components/index.js
@@ -1,3 +1,5 @@
-export { default as Navbar } from "./Navbar/Navbar";
-export { default as SocialMedia } from "./SocialMedia";
-export { default as NavigationDots } from "./NavigationDots";
+import NavigationDots from "./NavigationDots";
+import SocialMedia from "./SocialMedia";
+import Navbar from "./Navbar/Navbar";
+
+export { NavigationDots, SocialMedia, Navbar };
--- frontend_react/src/wrapper/index.js
+++ frontend_react/src/wrapper/index.js
@@ -1,2 +1,4 @@
-export { default as AppWrap } from "./AppWrap";
-export { default as MotionWrap } from "./MotionWrap";
+import AppWrap from "./AppWrap";
+import MotionWrap from "./MotionWrap";
+
+export { AppWrap, MotionWrap };
