4f4214c32744fad2039243cadf7a0740f0c01506
DESKTOP-2I0AT47\demon <dhowai.dev@gmail.com>
2023-02-09 15:28:04 +0000

    Change motion animations

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { motion } from "framer-motion";
 
 import { images } from "../../constants";
 import "./Navbar.scss";
+
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
@@ -24,16 +25,20 @@ const Navbar = () => {
         {toggle && (
           <motion.div
             whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: "easeOut" }}
+            transition={{ duration: 0.85, ease: "linear" }}
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
               {["home", "about", "work", "skills", "contact"].map((item) => (
-                <li key={item}>
+                <motion.li
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.95 }}
+                  key={item}
+                >
                   <a href={`#${item}`} onClick={() => setToggle(false)}>
                     {item}
                   </a>
-                </li>
+                </motion.li>
               ))}
             </ul>
           </motion.div>
